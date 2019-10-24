396aab7d95d98a30239525d2d90d0662a7e40670
ky82 <barwa@123>
2019-10-23 23:09:09 -0700

    modified friend pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 var friends = [
 	
 	{
-		name: "Muhammad Ali",
-		photo: "https://images.app.goo.gl/Mj47bqSJ4mTDjkUr6",
+		name: "Vladamir VanDamme",
+		photo: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
 		scores: [
 			"5",
 			"1",
@@ -17,8 +17,8 @@ var friends = [
 		]
 	},
 	{
-		name: "abraham lincoln",
-		photo: "https://images.app.goo.gl/SMQsHvmYcfKZUHJq8",
+		name: "Brian Eastman",
+		photo: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
 		scores: [
 			"1",
 			"1",
@@ -33,8 +33,8 @@ var friends = [
 		]
 	},
 	{
-		name: "Lebron James",
-		photo: "https://images.app.goo.gl/TocKGvqCMsjSZ5489",
+		name: "Aaron Rodgers",
+		photo: "http://static3.businessinsider.com/image/597a40094528e62c008b4ec6/aaron-rodgers-is-structuring-his-life-like-tom-brady-in-order-to-play-football-into-his-40s.jpg",
 		score: [
 			"3",
 			"3",
