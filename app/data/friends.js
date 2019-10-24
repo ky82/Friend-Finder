d4ddd4085a7de1f7e14e98a0c7cd54b65cd93669
ky82 <barwa@123>
2019-10-23 20:29:08 -0700

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Muhammad Ali",
+		photo: "https://images.app.goo.gl/Mj47bqSJ4mTDjkUr6",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "abraham lincoln",
+		photo: "https://images.app.goo.gl/SMQsHvmYcfKZUHJq8",
+		scores: [
+			"1",
+			"1",
+			"2",
+			"1",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Lebron James",
+		photo: "https://images.app.goo.gl/TocKGvqCMsjSZ5489",
+		score: [
+			"3",
+			"3",
+			"4",
+			"4",
+			"4",
+			"3",
+			"3",
+			"3",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
