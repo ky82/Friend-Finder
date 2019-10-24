9137d08b84f43dc34aa6c3e471147d08473b922b
ky82 <barwa@123>
2019-10-23 21:13:07 -0700

    Changes to the server file

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
